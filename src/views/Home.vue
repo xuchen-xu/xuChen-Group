f9976912d33e9a5065589397b8fab900bbb605f0
fmmmmmmmm <2727927026@qq.com>
2021-01-13 21:36:44 +0800

    1.13

---
+++ src/views/Home.vue
@@ -0,0 +1,275 @@
+<template>
+  <div class="app-container">
+    <div class="total-layout">
+      <el-row :gutter="20">
+        <el-col :span="6">
+          <div class="total-frame">
+            <img :src="img_home_order" class="total-icon">
+            <div class="total-title">今日订单总数</div>
+            <div class="total-value">200</div>
+          </div>
+        </el-col>
+        <el-col :span="6">
+          <div class="total-frame">
+            <img :src="img_home_today_amount" class="total-icon">
+            <div class="total-title">今日销售总额</div>
+            <div class="total-value">￥5000.00</div>
+          </div>
+        </el-col>
+        <el-col :span="6">
+          <div class="total-frame">
+            <img :src="img_home_yesterday_amount" class="total-icon">
+            <div class="total-title">昨日销售总额</div>
+            <div class="total-value">￥5000.00</div>
+          </div>
+        </el-col>
+      </el-row>
+    </div>
+    <div class="un-handle-layout">
+      <div class="layout-title">待处理事务</div>
+      <div class="un-handle-content">
+        <el-row :gutter="20">
+          <el-col :span="8">
+            <div class="un-handle-item">
+              <span class="font-medium">待付款订单</span>
+              <span style="float: right" class="color-danger">(10)</span>
+            </div>
+          </el-col>
+          <el-col :span="8">
+            <div class="un-handle-item">
+              <span class="font-medium">已完成订单</span>
+              <span style="float: right" class="color-danger">(10)</span>
+            </div>
+          </el-col>
+          <el-col :span="8">
+            <div class="un-handle-item">
+              <span class="font-medium">待确认收货订单</span>
+              <span style="float: right" class="color-danger">(10)</span>
+            </div>
+          </el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="8">
+            <div class="un-handle-item">
+              <span class="font-medium">待发货订单</span>
+              <span style="float: right" class="color-danger">(10)</span>
+            </div>
+          </el-col>
+          <el-col :span="8">
+            <div class="un-handle-item">
+              <span class="font-medium">新缺货登记</span>
+              <span style="float: right" class="color-danger">(10)</span>
+            </div>
+          </el-col>
+          <el-col :span="8">
+            <div class="un-handle-item">
+              <span class="font-medium">待处理退款申请</span>
+              <span style="float: right" class="color-danger">(10)</span>
+            </div>
+          </el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="8">
+            <div class="un-handle-item">
+              <span class="font-medium">已发货订单</span>
+              <span style="float: right" class="color-danger">(10)</span>
+            </div>
+          </el-col>
+          <el-col :span="8">
+            <div class="un-handle-item">
+              <span class="font-medium">待处理退货订单</span>
+              <span style="float: right" class="color-danger">(10)</span>
+            </div>
+          </el-col>
+          <el-col :span="8">
+            <div class="un-handle-item">
+              <span class="font-medium">广告位即将到期</span>
+              <span style="float: right" class="color-danger">(10)</span>
+            </div>
+          </el-col>
+        </el-row>
+      </div>
+    </div>
+    <div class="overview-layout">
+      <el-row :gutter="20">
+        <el-col :span="12">
+          <div class="out-border">
+            <div class="layout-title">商品总览</div>
+            <div style="padding: 40px">
+              <el-row>
+                <el-col :span="6" class="color-danger overview-item-value">100</el-col>
+                <el-col :span="6" class="color-danger overview-item-value">400</el-col>
+                <el-col :span="6" class="color-danger overview-item-value">50</el-col>
+                <el-col :span="6" class="color-danger overview-item-value">500</el-col>
+              </el-row>
+              <el-row class="font-medium">
+                <el-col :span="6" class="overview-item-title">已下架</el-col>
+                <el-col :span="6" class="overview-item-title">已上架</el-col>
+                <el-col :span="6" class="overview-item-title">库存紧张</el-col>
+                <el-col :span="6" class="overview-item-title">全部商品</el-col>
+              </el-row>
+            </div>
+          </div>
+        </el-col>
+        <el-col :span="12">
+          <div class="out-border">
+            <div class="layout-title">用户总览</div>
+            <div style="padding: 40px">
+              <el-row>
+                <el-col :span="6" class="color-danger overview-item-value">100</el-col>
+                <el-col :span="6" class="color-danger overview-item-value">200</el-col>
+                <el-col :span="6" class="color-danger overview-item-value">1000</el-col>
+                <el-col :span="6" class="color-danger overview-item-value">5000</el-col>
+              </el-row>
+              <el-row class="font-medium">
+                <el-col :span="6" class="overview-item-title">今日新增</el-col>
+                <el-col :span="6" class="overview-item-title">昨日新增</el-col>
+                <el-col :span="6" class="overview-item-title">本月新增</el-col>
+                <el-col :span="6" class="overview-item-title">会员总数</el-col>
+              </el-row>
+            </div>
+          </div>
+        </el-col>
+      </el-row>
+    </div>
+    <div class="statistics-layout">
+      <div class="layout-title">订单统计</div>
+      <el-row>
+        <el-col :span="4">
+          <div style="padding: 20px">
+            <div>
+              <div style="color: #909399;font-size: 14px">本月订单总数</div>
+              <div style="color: #606266;font-size: 24px;padding: 10px 0">10000</div>
+              <div>
+                <span class="color-success" style="font-size: 14px">+10%</span>
+                <span style="color: #C0C4CC;font-size: 14px">同比上月</span>
+              </div>
+            </div>
+            <div style="margin-top: 20px;">
+              <div style="color: #909399;font-size: 14px">本周订单总数</div>
+              <div style="color: #606266;font-size: 24px;padding: 10px 0">1000</div>
+              <div>
+                <span class="color-danger" style="font-size: 14px">-10%</span>
+                <span style="color: #C0C4CC;font-size: 14px">同比上周</span>
+              </div>
+            </div>
+            <div style="margin-top: 20px;">
+              <div style="color: #909399;font-size: 14px">本月销售总额</div>
+              <div style="color: #606266;font-size: 24px;padding: 10px 0">100000</div>
+              <div>
+                <span class="color-success" style="font-size: 14px">+10%</span>
+                <span style="color: #C0C4CC;font-size: 14px">同比上月</span>
+              </div>
+            </div>
+            <div style="margin-top: 20px;">
+              <div style="color: #909399;font-size: 14px">本周销售总额</div>
+              <div style="color: #606266;font-size: 24px;padding: 10px 0">50000</div>
+              <div>
+                <span class="color-danger" style="font-size: 14px">-10%</span>
+                <span style="color: #C0C4CC;font-size: 14px">同比上周</span>
+              </div>
+            </div>
+          </div>
+        </el-col>
+        <el-col :span="20">
+          <div style="padding: 10px;border-left:1px solid #DCDFE6">
+            <el-date-picker
+              style="float: right;z-index: 1"
+              size="small"
+              v-model="orderCountDate"
+              type="daterange"
+              align="right"
+              unlink-panels
+              range-separator="至"
+              start-placeholder="开始日期"
+              end-placeholder="结束日期"
+              @change="handleDateChange"
+              :picker-options="pickerOptions">
+            </el-date-picker>
+            <div>
+              <ve-line
+                :data="chartData"
+                :legend-visible="false"
+                :loading="loading"
+                :data-empty="dataEmpty"
+                :settings="chartSettings"></ve-line>
+            </div>
+          </div>
+        </el-col>
+      </el-row>
+    </div>
+  </div>
+</template>
+
+<script>
+import img_home_order from '@/assets/images/home_order.png';
+import img_home_today_amount from '@/assets/images/home_order.png';
+import img_home_yesterday_amount from '@/assets/images/home_yesterday_amount.png';
+export default ({
+  name:'home',
+  data(){
+    return{
+      img_home_order,
+      img_home_today_amount,
+      img_home_yesterday_amount
+    }
+  }
+})
+</script>
+<style lang="less" scoped>
+    .app-container {
+    margin-top: 40px;
+    margin-left: 120px;
+    margin-right: 120px;
+  }
+
+  .total-layout {
+    margin-top: 20px;
+  }
+
+  .total-frame {
+    border: 1px solid #DCDFE6;
+    padding: 20px;
+    height: 100px;
+  }
+
+  .total-icon {
+    color: #409EFF;
+    width: 60px;
+    height: 60px;
+  }
+
+  .total-title {
+    position: relative;
+    font-size: 16px;
+    color: #909399;
+    left: 70px;
+    top: -50px;
+  }
+
+  .total-value {
+    position: relative;
+    font-size: 18px;
+    color: #606266;
+    left: 70px;
+    top: -40px;
+  }
+  .un-handle-layout {
+    margin-top: 20px;
+    border: 1px solid #DCDFE6;
+  }
+  .layout-title {
+    color: #606266;
+    padding: 15px 20px;
+    background: #F2F6FC;
+    font-weight: bold;
+  }
+  .un-handle-content {
+    padding: 20px 40px;
+  }
+  .un-handle-item {
+    border-bottom: 1px solid #EBEEF5;
+    padding: 10px;
+  }
+
+</style>
